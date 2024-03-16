b2bc19ec0e49f3c12489ce583a5c863e5d2059da
mino <amine222@hotmail.nl>
2024-03-16 17:37:12 +0100

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
 import Benefits from './components/Benefits'
 import Collaboration from './components/Collaboration'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Pricing from './components/Pricing'
+import Roadmap from './components/Roadmap'
+import Services from './components/Services'
 
 const App = () => {
 
@@ -13,6 +17,10 @@ const App = () => {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
 
       <ButtonGradient />
